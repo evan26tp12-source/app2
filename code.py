14afe8a79354824538d7f7254a9a1ba6c560610d
mxmeclt <maxime.coulet@esme.fr>
2026-03-17 12:22:36 +0100

    Update code.py

--- code.py
+++ code.py
@@ -54,4 +54,4 @@ def tri_selection(trafic, policy): # on prend en paramètre la liste de tous les
 
     for i in range(n): 
 index_min = i #on dit que l'avion le plus prioritaire est a l'indice i
-#associer un score à un problème (+ le probleme est important plus le score est élevé) donc la somme définie la priorité
+#associer un score à un problème (+ le probleme est important plus le score est élevé) donc la somme définie la priorité)
